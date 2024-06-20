79a94db43c7d4f995b0efd5b2855eec2996a91ee
Anasch113 <anaschaudry2002@gmail.com>
2024-06-20 17:02:46 +0500

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
 console.log("Hello world");
+const getUsername = document.querySelector('#user');
+const formSubmit = document.querySelector('#form');
+const main_container = document.querySelector('.main-container');
+async function customFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`Error in fetching response and network error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+const showResultUi = (value) => {
+};
+function fetchUserData(url) {
+    customFetcher(url, {}).then((usersData) => {
+        for (const singleUser of usersData) {
+            showResultUi(singleUser);
+            console.log("single user", singleUser.id);
+        }
+    });
+}
+fetchUserData("https://api.github.com/users");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,54 @@
 console.log("Hello world")
+
+const getUsername = document.querySelector('#user') as HTMLInputElement
+const formSubmit = document.querySelector('#form') as HTMLFormElement
+const main_container = document.querySelector('.main-container') as HTMLElement
+
+
+interface UserData {
+
+    id: number;
+    login: string;
+    avatar_url: string;
+    location: string;
+    url: string;
+}
+
+
+
+
+async function customFetcher<T>(url: string, options?: RequestInit): Promise<T> {
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        throw new Error(`Error in fetching response and network error: ${response.status}`)
+    }
+
+    const data = await response.json();
+
+    return data
+
+
+}
+
+const showResultUi = (value: Object) => {
+
+}
+
+
+function fetchUserData(url: string) {
+
+    customFetcher<UserData[]>(url, {}).then((usersData) => {
+
+        for (const singleUser of usersData) {
+            showResultUi(singleUser)
+            console.log("single user", singleUser.id)
+        }
+
+    })
+
+}
+
+
+fetchUserData("https://api.github.com/users")
